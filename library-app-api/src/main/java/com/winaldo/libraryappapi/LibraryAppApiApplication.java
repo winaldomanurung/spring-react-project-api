9154c9747117f2c6a230f0452b8993a8d7d32da7
winaldomanurung <winaldo_satryadi@rocketmail.com>
2023-03-18 13:48:12 +0700

    first configuration

---
+++ library-app-api/src/main/java/com/winaldo/libraryappapi/LibraryAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.winaldo.libraryappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApiApplication.class, args);
+	}
+
+}
